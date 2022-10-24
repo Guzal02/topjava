bdf68fae86615ea0a94de2de9fa320fd20313ff8
Guzal <02guzal@gmail.com>
2022-10-24 16:39:33 +0300

    HW03_2_Supplemented the scripts for creating and initializing the database with the MEALS table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,6 +2,8 @@ DELETE
 FROM user_roles;
 DELETE
 FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -12,3 +14,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2022-10-20 15:10:45.120120', 'Картошка', 200),
+       (100000, '2022-10-22 12:15:32.256248', 'Обед', 250),
+       (100000, '2022-10-02 09:13:15.123698', 'Завтрак', 345),
+       (100001, '2022-10-15 17:55:36.963197', 'Перекус', 220),
+       (100001, '2022-10-06 20:01:28.698573', 'Мясо', 550),
+       (100001, '2022-10-05 18:02:19.123321', 'Суп', 150);
