b9db64dcd597c93face80fea665be7fb403b0fee
Guzal <02guzal@gmail.com>
2022-11-09 22:45:23 +0300

    HW05_3_Made tests of all implementations (jdbc, jpa, datajpa) through inheritance without duplication

---
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserRepositoryTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service.jdbc;
+
+import org.springframework.jdbc.core.support.JdbcBeanDefinitionReader;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+import static ru.javawebinar.topjava.Profiles.JDBC;
+
+@ActiveProfiles(JDBC)
+public class JdbcUserRepositoryTest extends AbstractUserServiceTest {
+}
