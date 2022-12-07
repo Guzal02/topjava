a04cd55ed87e71375184f8a8cf41f8f81415f5ea
Guzal <02guzal@gmail.com>
2022-12-07 20:03:19 +0300

    HW08_1_Transferred meals to Datatables (changed meals.jsp, instead of JspMealController now MealUIController)

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.model.to.MealTo;
 
 import java.time.Month;
 import java.util.List;
